cc598f6421f7ff0c2c9506b77245c0e56c80dd91
QuantumSolver <marcquesmouton1992@gmail.com>
2025-12-11 17:07:40 +0000

    feat: export all orphaned doctypes to prevent deletion during migrate
    Exported 105 custom doctypes that existed only in DB without files:
    - 86 custom doctypes (custom=1)
    - 19 non-custom doctypes that were missing files
    
    These doctypes are now in WCFCB Customizations module and will survive
    bench migrations without being deleted as orphans.

---
+++ wcfcb_customizations/wcfcb_customizations/doctype/employer/employer.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, WCFCB and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class Employer(Document):
+	pass
