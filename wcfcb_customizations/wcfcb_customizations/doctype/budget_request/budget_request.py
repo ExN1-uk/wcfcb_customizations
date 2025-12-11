487b9d81dd40e2b93ec57e4af68738f28e1fb5ae
QuantumSolver <marcquesmouton1992@gmail.com>
2025-12-11 18:50:59 +0000

    feat: export Budget Request doctype

---
+++ wcfcb_customizations/wcfcb_customizations/doctype/budget_request/budget_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, WCFCB and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class BudgetRequest(Document):
+pass
